bdac9ea681b64cff32d1c49d090718b2220de40a
Your Name <you@example.com>
2019-04-05 15:29:04 +0200

    add contacts and php

--- contact_me.php
+++ contact_me.php
@@ -22,7 +22,6 @@ $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: inmgr2018@gmail.com\r\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address\r\n";   
-
 mail($to, $email_subject, $email_body, $headers);
 exit();
 ?>
