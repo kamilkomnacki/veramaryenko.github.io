f630fde960c971aec2c10a3012ab1d2ca932f901
Marek Grzelak <marek10070@gmail.com>
2019-04-09 23:54:28 +0200

    add menu tests 1-5

---
+++ automatedtests/ClassLibrary1/MenuTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using System.Runtime;
+using System.Threading;
+
+namespace PageObjectsTest.menutests
+{
+    [TestFixture]
+    public class MenuTests
+    {
+        private VirlekiPage page = new VirlekiPage();
+
+        public static void Main(string[] args)
+        {
+
+        }
+
+
+        [SetUp]
+        public void TestSetup()
+        {
+            page.Load();
+            //page.GetDriver().Manage().Window.FullScreen();
+
+
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            //page.Quit();
+        }
+
+
+        [Test]
+        public void AboutMenuTest()
+        {
+            //Thread.Sleep(3000);
+            Assert.True(page.ClickElement(By.XPath("//*[@id='services']/div/div/div/h2")));
+            Assert.True(page.IsElementVisible(By.XPath("//*[@id='services']/div/div/div/h2")));
+        }
+
+        [Test]
+        public void HistoryMenuTest()
+        {
+            //Thread.Sleep(3000);
+            Assert.True(page.ClickElement(By.XPath("//*[@id='about']/div/div[1]/div/h2")));
+            Assert.True(page.IsElementVisible(By.XPath("//*[@id='about']/div/div[1]/div/h2")));
+        }
+
+        [Test]
+        public void BackgroundMenuTest()
+        {
+            //Thread.Sleep(3000);
+            Assert.True(page.ClickElement(By.XPath("//*[@id='background']/div/div[1]/div/h2")));
+            Assert.True(page.IsElementVisible(By.XPath("//*[@id='background']/div/div[1]/div/h2")));
+        }
+
+        [Test]
+        public void ProductMenuTest()
+        {
+            //Thread.Sleep(3000);
+            Assert.True(page.ClickElement(By.XPath("//*[@id='product']/div/div[1]/div/h2")));
+            Assert.True(page.IsElementVisible(By.XPath("//*[@id='product']/div/div[1]/div/h2")));
+        }
+
+        [Test]
+        public void TeamMenuTest()
+        {
+            //Thread.Sleep(3000);
+            Assert.True(page.ClickElement(By.XPath("//*[@id='navbarResponsive']/ul/li[5]/a")));
+            Assert.True(page.IsElementVisible(By.XPath("//*[@id='navbarResponsive']/ul/li[5]/a")));
+        }
+
+
+    }
+}
