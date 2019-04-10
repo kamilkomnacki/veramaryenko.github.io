82338cf94348154658c85873162109657e2c1714
veramaryenko <verunga15@mail.ru>
2019-04-10 06:20:58 +0200

    vm tests

---
+++ automatedTestsVirLeki/automatedTestsVirLeki/PageObjects/NavBar.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace autoTestsVirleki.PageObjects
+{
+    class NavBar
+    {
+        private readonly IWebDriver webDriver;
+        private readonly string url = @"https://veramaryenko.github.io/bootstrap/";
+
+        public NavBar(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        protected NavBarElementsMap Map
+        {
+            get
+            {
+                return new NavBarElementsMap(this.webDriver);
+            }
+        }
+
+        public void LogoButtonClick()
+        {
+            this.Map.LogoButton.Click();
+        }
+    }
+}
---
+++ automatedTestsVirLeki/automatedTestsVirLeki/Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using autoTestsVirleki.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace automatedTestsVirLeki
+{
+    [TestClass]
+    public class Tests
+    {
+        public RemoteWebDriver Driver { get; set; }
+        static NavBar navBar;
+
+        [TestInitialize]
+        public void SetupTests()
+        {
+            this.Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            navBar = new NavBar(this.Driver);
+        }
+
+        [TestCleanup]
+        public void TearDownTest()
+        {
+            this.Driver.Quit();
+        }
+
+        [TestMethod]
+        public void LogoButtonClick()
+        {
+            navBar.LogoButtonClick();
+
+        }
+    }
+}
