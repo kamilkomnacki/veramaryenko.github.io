a39368ede7f38ba1a006fcaeeaba2b64bf6e55a3
veramaryenko <verunga15@mail.ru>
2019-04-10 06:20:58 +0200

    vm tests

---
+++ automatedTestsVirLeki/automatedTestsVirLeki/PageObjects/NavBarElementsMap.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace autoTestsVirleki.PageObjects
+{
+    class NavBarElementsMap
+    {
+        private readonly IWebDriver webDriver;
+
+        public NavBarElementsMap(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+        }
+
+        public IWebElement LogoButton
+        {
+            get
+            {
+                return this.webDriver.FindElement(By.XPath("//a[@class='navbar-brand js-scroll-trigger text-uppercase']"));
+            }
+        }
+    }
+}
---
+++ automatedTestsVirLeki/automatedTestsVirLeki/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("automatedTestsVirLeki")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("automatedTestsVirLeki")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4257a42a-1cc6-447c-9461-b7b6b7534770")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
